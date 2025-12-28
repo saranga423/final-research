3638dbf67100245b12ae5d41f5a2c8066a7d5883
Saranga Rasingolla <162134044+saranga423@users.noreply.github.com>
2025-12-28 21:39:26 +0530

    Delete backend-api directory

--- backend-api/models/Flower.js
+++
@@ -1,9 +0,0 @@
-//backend-api/models/flower.js
-const mongoose = require('mongoose');
-const FlowerSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  color: { type: String, required: true },
-  species: { type: String, required: true },
-  createdAt: { type: Date, default: Date.now }
-});
-module.exports = mongoose.model('Flower', FlowerSchema);
